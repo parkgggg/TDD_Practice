a318c620c6c8c30ad317d93047fd64259361e6a5
gupark <ghp720@naver.com>
2024-01-24 17:13:28 +0900

    inital

--- src/App.js
+++ src/App.js
@@ -1,22 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  //주석 처리한 건 내가 짠 코드 (같은 원리로 돌아간다)
+
+  /*const handleClick = (e) => {
+    //엘리먼트 name을 구분해서 분기해준다.
+    if (!isDisabled) {
+      if (e.target.name === "minus") {
+        setCount((count) => count - 1);
+      } else {
+        setCount((count) => count + 1);
+      }
+    }
+  };
+
+  const handleDisabled = () => {
+    if (isDisabled) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  };*/
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/*}
+        <h3 data-testid="counter">{count}</h3>
+        <div>
+          <button data-testid="minus-button" disabled={isDisabled} name="minus" onClick={handleClick}>
+            -
+          </button>
+          <button data-testid="plus-button" disabled={isDisabled} name="plus" onClick={handleClick}>
+            +
+          </button>
+        </div>
+        <div>
+          <button
+            style={{ backgroundColor: "blue" }}
+            data-testid="onoff-button"
+            onClick={handleDisabled}
+          >
+            Click
+          </button>
+  </div>*/}
+
+        <h3 data-testid="counter">{count}</h3>
+        <div>
+          <button
+            data-testid="minus-button"
+            disabled={isDisabled}
+            name="minus"
+            onClick={() => setCount((prev) => prev - 1)}
+          >
+            -
+          </button>
+          <button
+            data-testid="plus-button"
+            disabled={isDisabled}
+            name="plus"
+            onClick={() => setCount((prev) => prev + 1)}
+          >
+            +
+          </button>
+        </div>
+        <div>
+          <button
+            style={{ backgroundColor: "blue" }}
+            data-testid="onoff-button"
+            onClick={() => setIsDisabled((  qprev) => !prev)}
+          >
+            Click
+          </button>
+        </div>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,51 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('the counter starts at 0', () => {
+  //APP 컴포넌트 렌더링
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  //Screen object를 이용해서 원하는 엘레멘트에 접근 (접근할 때 ID로 접근)
+  const counterElement = screen.getByTestId("counter");
+  //ID가 counter인 엘레멘트의 텍스트가 0인지 테스트
+  expect(counterElement).toHaveTextContent(0);
 });
+
+test('minus button has correct text', () => {
+    render(<App />);
+    const buttonElement = screen.getByTestId("minus-button");
+    //ID가 counter인 엘레멘트의 텍스트가 "-"인지 테스트
+    expect(buttonElement).toHaveTextContent("-");
+})
+
+test('plus button has correct text', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("plus-button");
+  //ID가 counter인 엘레멘트의 텍스트가 "+"인지 테스트
+  expect(buttonElement).toHaveTextContent("+");
+})
+
+test('When the + button pressed, the counter changes to 1', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("plus-button");
+  //buttonElement에 대해 클릭 이벤트를 발생시킨다(테스트를 위해)
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId("counter");
+  //0이었던 count가 1로 변하는지 체크
+  expect(counterElement).toHaveTextContent(1);
+})
+
+test('Is onoff button color blue', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("onoff-button");
+  expect(buttonElement).toHaveStyle({backgroundColor: "blue"});
+})
+
+test('When onoff button clicked, +- button being disabled.', () => {
+  render(<App />);
+  const onoffButton = screen.getByTestId("onoff-button");
+  fireEvent.click(onoffButton);
+  const plusButton = screen.getByTestId("plus-button");
+  expect(plusButton).toBeDisabled();
+})
+
+
